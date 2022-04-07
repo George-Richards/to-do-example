0815fa47af3ffb7a45c32a76b22482e2f5d95d1c
George-Richards <georgerichards1998@icloud.com>
2022-04-07 13:32:53 +0000

    added tasks to homepage

--- application/routes.py
+++ application/routes.py
@@ -1,12 +1,21 @@
-from application import app
+from application import app, db
 from application.forms import TaskForm
-from flask import render_template
+from application.models import Tasks
+from flask import render_template, request, redirect, url_for
 
 @app.route('/')
 def index():
-    return render_template('index.html')
+    all_tasks = Tasks.query.all()
+    return render_template('index.html', all_tasks=all_tasks)
 
 @app.route('/add_task', methods=['GET', 'POST'])
 def add_task():
     form = TaskForm()
+    if request.method == "POST":
+        task = Tasks(task_title=form.task_title.data,
+        description=form.description.data)
+        db.session.add(task)
+        db.session.commit()
+        return redirect(url_for('index'))
+
     return render_template('add_task.html', form=form)
